9c8ed6bfe40e2fa8ecae6e23192d8dea61dae766
eugbor <eugeneborodin@mail.ru>
2016-09-12 00:40:45 +0300

    init

---
+++ PrimeNumber/Number.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeNumber
+{
+    class Number
+    {
+        public int IsPrime(int x)
+        {
+            for (int i = 2; i <= x/i; i++)
+                if ((x % i) == 0) return -1;
+            return x;
+        }
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var ob = new Number();
+            for (int i = 2; i <= n; i++)
+            {
+                if(ob.IsPrime(i) != -1)
+                Console.WriteLine(ob.IsPrime(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
